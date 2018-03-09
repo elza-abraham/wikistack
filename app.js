3a3f1ed1864cc91795255792f7b0a646bb32a3a6
eugenepark2014 <eugenepark2014@gmail.com>
2018-03-09 12:24:07 -0500

    app connects to local host

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('test');
+const express = require("express");
+
+const app = express();
+const{db} =  require('./models');
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+db.authenticate().then(() => {console.log('connected to the database')})
+const PORT = 3000;
+
+app.listen(PORT, () =>{
+  console.log(`App listening in port ${PORT}`);
+});
