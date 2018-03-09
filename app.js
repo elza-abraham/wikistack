85022918af7fcb96599eaf4e88b0c33d508122e7
eugenepark2014 <eugenepark2014@gmail.com>
2018-03-09 12:52:35 -0500

    before lunch

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require("express");
 
 const app = express();
-const{db} =  require('./models');
+const models = require('./models');
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
-db.authenticate().then(() => {console.log('connected to the database')})
-const PORT = 3000;
+const init = async () => {
+  await models.User.sync()
+  await models.Page.sync()
+  const PORT = 3000;
+  app.listen(PORT, () => {
+    console.log(`App listening in port ${PORT}`);
+  });
+}
 
-app.listen(PORT, () =>{
-  console.log(`App listening in port ${PORT}`);
-});
+init();
--- models/index.js
+++ models/index.js
@@ -1,8 +1,26 @@
 const Sequelize = require("sequelize");
 const db = new Sequelize('postgres://localhost:5432/wikistack')
 
-module.exports = {
-  db
-}
+const Page = db.define('Page', {
+  name: {
+    type: Sequelize.STRING
+  },
+  slug: {
+    type: Sequelize.STRING
+  },
+  content: {
+    type: Sequelize.TEXT
+  }
+  ,
+  status: {
+    type: Sequelize.ENUM('open', 'closed')
+  }
 
-const page = db.define('Page', {name: Sequelize.STRING, slug:)
+})
+const User = db.define('User',
+  {
+    name: { type: Sequelize.STRING },
+    email: { type: Sequelize.STRING }
+  })
+
+module.exports = { Page, User };
